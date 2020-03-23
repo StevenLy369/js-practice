27348d9227d5ba1b33ccf7678b701d8bde75d5da
Steven Ly <1stevenly92@gmail.com>
2020-03-23 09:17:50 -0700

    did first js practice

---
+++ script.js
@@ -0,0 +1,26 @@
+// alert("DANGER");
+
+// confirm("Are u shure?");
+
+
+// prompt("Are you feeling well");
+// var someRandon = prompt("yor favourite colour");
+// someRandon;
+// console.log(someRandon);
+
+// console.log(someRandon.toUpperCase);
+
+// var inputAge = prompt("youe age");
+// var newAege = parseInt(inputAge);
+// console.log(newAege);
+
+// var name= prompt("Enter your name");
+
+
+
+
+
+// var name = confirm("My name is");
+
+// console.log("my name is " + name);
+
