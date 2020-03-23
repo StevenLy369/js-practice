5633b31373e22595abae77b7fa6df3cacb4be461
Steven Ly <1stevenly92@gmail.com>
2020-03-23 11:21:00 -0700

    finished second practice

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // alert("DANGER");
 
+// import { func } from "prop-types";
+
 // confirm("Are u shure?");
 
 
@@ -23,4 +25,145 @@
 // var name = confirm("My name is");
 
 // console.log("my name is " + name);
+
+
+
+// function wow(){
+//     alert("Hi")
+// }
+
+// wow();
+
+//function add(number1, number2) {
+ // return number1 + number2;
+//};
+//add(3, 5);
+
+//console.log(add(3, 5));
+//console.log(typeof(add));
+
+// var newName = function typeName() {
+//   prompt("Type your name");
+// }
+// typeName();
+
+// console.log(typeName);
+
+
+
+
+// function greeting(){
+//     prompt("type your name");
+//     // return greeting();
+// }
+
+// var newName = function greeting1(){
+//   prompt("Type your name");
+
+// }
+// greeting1();
+
+// function newFunction(){
+//   alert("hi" + newName);
+// }
+// newFunction();
+
+// var newName1 = prompt("enter your name");
+
+// function newFunction2(){
+//     alert(("hi"+ " " + newName1 ));
+// }
+// newFunction2(newName1);
+
+// var num1 = prompt ("type your first number");
+// var num2 = prompt ("type your second number");
+// function newFunction() {
+//   var newValue = parseInt.num1 - parseInt.num2;
+//   return newValue;
+//   alert("The result is " + newValue); 
+// }
+
+// // console.log(num1);
+// // console.log(num2);
+
+
+
+// var newFunction1 = function(outPut){
+//     alert(outPut)
+// };
+
+
+// newFunction1(newFunction);
+
+
+// var num1 = prompt ("type your first number");
+// var num2 = prompt ("type your second number");
+// function addNewFunction(num1, num2) {
+//   parseInt.num1;
+//   parseInt.num2;
+//   //var result = num1 - num2;
+//    return addNewFunction;
+// }
+
+// var result = num1 - num2;
+// addNewFunction(num1, num2);
+// alert ("the result is " + result);
+
+// var num1 = prompt("enter your first number");
+// var num2 = prompt ("enter your second number");
+
+
+// function threetimes(num1, num2){
+//     return num1 * num2;
+// }
+
+//  var result = threetimes(num1, num2);
+// /
+//  alert(result);
+
+// var num1 = prompt("enter your first number");
+// var num2 = prompt ("enter your second number");
+
+
+// function divide(num1, num2){
+//     return num1 % num2;
+// }
+
+//  var result = divide(num1, num2);
+
+//  alert(result);
+
+
+var age = prompt("Enter your age");
+var name = prompt("Enter your name");
+var food = prompt("Enter some food");
+
+function output(age, food, name){
+    
+alert("My name is " + name + " I am " + age + "and I love " + food );
+    return age, food, name;
+    
+
+}
+output(age, food, name);
+
+
+
+
+
+
+// function threetimes(){
+// parseInt.num1;
+// parseInt.num2;
+
+// return threetimes;
+
+//}
+// var result = num1 * num2;
+// threetimes(num1, num2);
+// alert(result);
+
+
+
+
 
